ed70bff34f68a1f8419bef9737f7609335deede8
Benjamin Mikkelsen <BenjaminMikkelsen@example.com>
2023-02-16 21:25:38 +0100

    Updated documentation.
    Made code a bit more readable.

--- Easee-Script.py
+++ Easee-Script.py
@@ -1,4 +1,5 @@
 # %%
+import eloverblik
 import requests
 import json
 import numpy as np
@@ -8,27 +9,31 @@ from datetime import timedelta
 
 pd.set_option('mode.chained_assignment', None)
 
-defaultYear = dt.today().year
+today = dt.today()
+defaultYear = today.year
+
 print('Due to limitations in the Easee API, the period must be scoped to a single month.')
-year = input('Enter a year (it will defualt to ' + str(defaultYear) +'): ')
-startMonth = int(input('Enter a month (1-12): '))
+try:
+    year = int(
+        input(f"Enter a year (it will default to {defaultYear}): ") or defaultYear)
+    startMonth = int(input('Enter a month (1-12): '))
+except ValueError:
+    print("Invalid input. Please enter an integer.")
 
-year = int(year) if year else defaultYear
-
-if(int(year) == dt.today().year and startMonth > dt.today().month):
+if (int(year) == today.year and startMonth > today.month):
     print("The input date cannot be bigger than the current month and year")
     exit()
 
 # %%
 # Basic setup - Set the period
-startDate = dt.today().date().replace(day=1,month=startMonth,year=year)
-if(startDate.month == 12):
-    endDate = dt.today().date().replace(year=startDate.year+1, month=1, day=1)
-else:
-    endDate = dt.today().date().replace(month=startDate.month+1, day=1)
+startDate = today.replace(day=1, month=startMonth, year=year)
+endDate = today.replace(year=startDate.year+1, month=1,
+                        day=1) if startDate.month == 12 else today.replace(month=startDate.month+1, day=1)
+
 
 def getDateAsString(date):
-    return date.strftime("%Y/%m/%d %H:%M:%S").replace('/','-')
+    return date.strftime("%Y/%m/%d %H:%M:%S").replace('/', '-')
+
 
 # %%
 # Login to Easee with credentials to get an Access Token
@@ -37,8 +42,8 @@ with open("configuration.json") as file:
 url = "https://api.easee.cloud/api/accounts/login"
 
 payload = {
-        "userName": config['Easee']['Username'], 
-        "password": config['Easee']['Password']
+    "userName": config['Easee']['Username'],
+    "password": config['Easee']['Password']
 }
 
 headers = {
@@ -58,7 +63,7 @@ headers = {
 }
 
 response = requests.get(url, headers=headers)
-if(not response.ok):
+if (not response.ok):
     print('Easee API could not be called: ' + response.reason)
     exit()
 
@@ -83,42 +88,47 @@ dates = pd.to_datetime(stringDates, format='%Y-%m-%d %H:%M:%S.%f', utc=True)
 dates = dates.tolist()
 kWhMeasurements = [item['consumption'] for item in resWithMeasurements]
 
-df = pd.DataFrame({"Dato": pd.to_datetime(dates,utc=True), "KwH": kWhMeasurements})
-if(df.empty):
+df = pd.DataFrame({"Dato": pd.to_datetime(
+    dates, utc=True), "kWh": kWhMeasurements})
+if (df.empty):
     print('No data could be found from the Easee API')
     exit()
 
 
 # %%
 # Get Tariff data
-import eloverblik
 dfTotal = eloverblik.getData(config['Eloverblik']['Token'], startDate, endDate)
-if(dfTotal is None):
+if (dfTotal is None):
     exit()
 
 # %%
 
-#Merge data based upon the timestamp
+# Merge data based upon the timestamp
 merge = pd.merge(df, dfTotal, how='outer', left_on='Dato', right_on='HourDK')
 
-#Only take data where there's a measurement 
-mergeRelData = merge[merge['KwH'] > 0]
+# Only take data where there's a measurement
+mergeRelData = merge[merge['kWh'] > 0]
 
 
 # Setup variables
-mergeRelData['Charge_Price'] = np.round(mergeRelData['KwH'] * mergeRelData['Total_Price'],2)
+mergeRelData['Charge_Price'] = np.round(
+    mergeRelData['kWh'] * mergeRelData['Total_Price'], 2)
 mergeRelData = mergeRelData.drop(columns=['HourDK'])
 mergeRelData['short_date'] = mergeRelData['Dato'].dt.date
 mergeRelData['short_date'] = mergeRelData['short_date'].astype('datetime64')
 
-mergeRelData.to_csv('output.csv', index=False) 
+
+file_name = f"{startDate.strftime('%Y-%m-%d')}_{endDate.strftime('%Y-%m-%d')}.csv"
+mergeRelData.to_csv(file_name, index=False)
 
 # %%
 # Display price usage pr. day in the given month
 
 print('_________________________________________________________________')
-print('Period: ' + startDate.strftime('%d/%m/%Y') + ' - ' + endDate.strftime('%d/%m/%Y'))
-print('Total pris (DKK): ' + str(sum(mergeRelData['Charge_Price'])))
-print('Total kWh: ' + str(sum(mergeRelData['KwH'])))
-print('Avg DKK/kWh: ' + str(sum( mergeRelData['Charge_Price']/sum(mergeRelData['KwH']))))
+print('Period: ' + startDate.strftime('%d/%m/%Y') +
+      ' - ' + endDate.strftime('%d/%m/%Y'))
+print('Total price (DKK): ' + str(sum(mergeRelData['Charge_Price'])))
+print('Total kWh: ' + str(sum(mergeRelData['kWh'])))
+print('Avg DKK/kWh: ' +
+      str(sum(mergeRelData['Charge_Price']/sum(mergeRelData['kWh']))))
 print('_________________________________________________________________')
--- readme.md
+++ readme.md
@@ -2,18 +2,19 @@
 
 This code makes it possible to extract data from an Easee Home Electrical Vichicle Charger and Eloverblik.
 
-This allows you see the actual price for charging your EV in a given month.
+The script will ask for a year and a month.
 
-The output, when run in Visual Studio Code, looks like this
+The output, when run in Visual Studio Code, looks like this:
 
 ![Output of the script](screenshots/output.png)
 
 ## Setup
-Create a file named ```configuration.json``` in the root folder of the project.
+
+Create a file named `configuration.json` in the root folder of the project.
 
 The files content should look like this:
 
-```
+```json
 {
     "Easee": {
         "Username": "EASEE_HOME_USERNAME",
@@ -33,22 +34,23 @@ Where EASEE_HOME_USERNAME and EASEE_HOME_PASSWORD is your login credentials to h
 EASEE_HOME_CHARGER_NAME is the name of your charger found on the Easee Cloud Portal (https://easee.cloud/products).
 
 TOKEN_FROM_ELOVERBIK is a token that can be required from https://eloverblik.dk/.
-This token is responsible for reading the getting the tarifss associated with your area.
+This token is responsible for reading the tariffs associated with your area.
 
 ## Running the script
+
 This script was developed using Python 3.9.4.
 
-### Run the following commands 
+### Run the following commands
 
 Setup virtual environment:
-```python -m venv venv```
+`python -m venv venv`
 
-Activate the environment: 
-```venv\bin\activate``` (There are several activate files. Use the one the mathces your environment - eg. the ps1 file for PowerShell)
+Activate the environment:
+`venv\bin\activate` (There are several activate files. Use the one the mathces your environment - eg. the ps1 file for PowerShell)
 
-Install the requirements: ```pip install -r requirements.txt```
+Install the requirements: `pip install -r requirements.txt`
 
-Run the script using this command ```python Easee-Script.py```.
+Run the script using this command `python Easee-Script.py`.
 
-Running the script will output the data in the console windows - and save the measurements to a .csv-file named ```output.csv```.
+Running the script will output the data in the console windows - and save the measurements to a .csv-file named `START-PERIOD_END-PERIOD.csv` eg. `2023-01-01_2023-02-01.csv`.
 ![CSV File](screenshots/output_csv.png)
